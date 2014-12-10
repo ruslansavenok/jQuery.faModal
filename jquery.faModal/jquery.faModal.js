eba3772e4788a8ef9d95ba877c3ff1ab5aae3d0f
Ruslan <ruslan.savenok@gmail.com>
2014-12-11 00:40:37 +0100

    The plugin and the demo

---
+++ jquery.faModal/jquery.faModal.js
@@ -0,0 +1,67 @@
+﻿(function ($) {
+
+  var defaultOptions = {
+    template: '<div class="fa-modal__overlay">' +
+                '<div class="fa-modal__content-container">' +
+                '</div>' +
+              '</div>'
+  }
+
+
+
+  /*  Modal Class
+   */
+  var Modal = function ($modalContent, options) {
+    var modal = this;
+    var options = $.extend({}, defaultOptions, options);
+    var $modal = $(options.template);
+    var $modalCloseBtn = $modal.find('.fa-modal__close-btn');
+    var $modalContentContainer = $modal.find('.fa-modal__content-container');
+
+    $modal.appendTo('body');
+    $modalContentContainer.html($modalContent);
+
+    $modal.add($modalCloseBtn).on('click', function () {
+      $modal.trigger('hide');
+    });
+    $modalContent.on('click', function (e) {
+      e.stopImmediatePropagation()
+    });
+
+    $modal.on({
+      show: function () {
+        $modal.fadeIn().addClass('fa-modal__overlay_visible');
+      },
+      hide: function () {
+        $modal.fadeOut().removeClass('fa-modal__overlay_visible');
+      }
+    });
+
+    return {
+      options: options
+    }
+  }
+
+
+
+  /*  jQuery Plugin API
+   */
+  $.fn.faModal = function (options) {
+    if (typeof options == 'string') {
+      return this.trigger(options);
+    }
+
+    return this.each(function () {
+      var $this = $(this);
+      var modal = $this.data('fa-modal');
+
+      if (modal) {
+        $.extend(modal.options, options);
+      } else {
+        $this.data('fa-modal', new Modal($this, options));
+      }
+    });
+  }
+
+
+})(jQuery);
